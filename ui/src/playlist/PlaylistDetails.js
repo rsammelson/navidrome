ac89d13a7fa664b69b3922722badede518c01f20
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2022-03-20 13:48:19 -0400

    feat(playlists): optionally add ability to show, filter duplicates
    - Add new configuration option EnableDuplicateSearch (default false)
    - Add a new search field duplicate. This is not a new column, but is constructed with a common table expression
    - Modify UI to support showing/filtering
    
    Signed-off-by: Kendall Garner <17521368+kgarner7@users.noreply.github.com>

--- ui/src/playlist/PlaylistDetails.js
+++ ui/src/playlist/PlaylistDetails.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Card, CardContent, Typography } from '@material-ui/core'
+import {
+  Card,
+  CardContent,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { useTranslate } from 'react-admin'
 import { DurationField, SizeField } from '../common'
-import Linkify from '../common/Linkify'
+import config from '../config'
 
 const useStyles = makeStyles(
   (theme) => ({
@@ -71,6 +77,15 @@ const PlaylistDetails = (props) => {
             <span>&nbsp;</span>
           )}
         </Typography>
+        {config.enableDuplicateSearch && (
+          <Typography component="p">
+            <FormControlLabel
+              onChange={(_event, checked) => props.setShowDuplicates(checked)}
+              control={<Checkbox />}
+              label={translate('resources.playlist.actions.showDuplicates')}
+            />
+          </Typography>
+        )}
       </CardContent>
     </Card>
   )
